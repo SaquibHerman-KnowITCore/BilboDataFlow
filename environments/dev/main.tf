6cef33b64c3609c2059a3ef2904934c1293f0bf6
Saquib Herman <saquib.herman@knowit.se>
2025-05-21 23:28:19 +0200

    Initial Commit

---
+++ environments/dev/main.tf
@@ -0,0 +1,45 @@
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.application_name}-${var.environment_name}"
+  location = var.primary_location
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "st${var.application_name}${var.environment_name}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${var.application_name}-${var.environment_name}"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+
+  name                      = "kv-${var.application_name}-${var.environment_name}"
+  location                  = azurerm_resource_group.main.location
+  resource_group_name       = azurerm_resource_group.main.name
+  tenant_id                 = data.azurerm_client_config.current.tenant_id
+  sku_name                  = "standard"
+  enable_rbac_authorization = true
+
+}
+
+resource "azurerm_role_assignment" "terraform_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
---
+++ scripts/debug-stage.sh
@@ -0,0 +1,25 @@
+# set the subscription
+export ARM_SUBSCRIPTION_ID="e1b4bba1-73df-4932-96ca-85ee85fec54a"
+
+# set the application / environment
+export TF_VAR_application_name="bilbodataflow"
+export TF_VAR_environment_name="stage"
+
+# set the backend
+export BACKEND_RESOURCE_GROUP="rg-bilbodataflow-stage"
+export BACKEND_STORAGE_ACCOUNT="stbilbodataflowstage"
+export BACKEND_STORAGE_CONTAINER="tfstate"
+export BACKEND_KEY=$TF_VAR_application_name-$TF_VAR_environment_name
+
+# run terraform
+terraform init \
+    -backend-config="resource_group_name=${BACKEND_RESOURCE_GROUP}" \
+    -backend-config="storage_account_name=${BACKEND_STORAGE_ACCOUNT}" \
+    -backend-config="container_name=${BACKEND_STORAGE_CONTAINER}" \
+    -backend-config="key=${BACKEND_KEY}" \
+    -reconfigure
+
+terraform $*
+
+# clean up the local environment
+rm -rf .terraform
