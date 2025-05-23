a94a05ef52813f5ba5c82e4f3f37c0823c40b72c
Saquib Herman <saquib.herman@knowit.se>
2025-05-23 11:32:16 +0200

    remove terraform user role assignment from script

--- environments/dev/bronze/main.tf
+++ environments/dev/bronze/main.tf
@@ -38,8 +38,10 @@ resource "azurerm_key_vault" "main" {
 
 }
 
+/*
 resource "azurerm_role_assignment" "terraform_user" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+*/
