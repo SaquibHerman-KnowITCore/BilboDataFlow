478d325e7d42a367e08482c21f424e90653b32df
Saquib Herman <saquib.herman@knowit.se>
2025-05-25 16:23:05 +0200

    Add new folders in DataLake

--- bronze/main.tf
+++ bronze/main.tf
@@ -9,12 +9,31 @@ resource "azurerm_storage_account" "main" {
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  is_hns_enabled           = true
+  tags = {
+    environment = var.environment_name
+    project     = var.application_name
+    component   = "storage"
+  }
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_id    = azurerm_storage_account.main.id
   container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "bronze_ingestion" {
+  name                  = "bronze"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "kobra_ingestion_folder" {
+  path               = "kobra"
+  filesystem_name    = azurerm_storage_container.bronze_ingestion.name
+  storage_account_id = azurerm_storage_account.main.id
+  resource           = "directory"
 }
 
 resource "azurerm_log_analytics_workspace" "main" {
@@ -37,3 +56,4 @@ resource "azurerm_key_vault" "main" {
   enable_rbac_authorization = true
 
 }
+
